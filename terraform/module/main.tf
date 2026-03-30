0e3e6910f17f904f5ef2d1c69ca0fb414b3e1cbe
harishmurali10725-beep <harishmurali10725@gmail.com>
2026-03-30 10:03:27 +0000

    path changed

--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -6,5 +6,5 @@ provider "aws"{
 resource "aws_s3_object" "json_upload" {
   bucket = var.bucket_name
   key    = "log1.txt"
-  source = "../../../log-file/log1.txt"
+  source = "../../log-file/log1.txt"
 }
